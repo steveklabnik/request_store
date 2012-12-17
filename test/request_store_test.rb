47f09173fc4eb857d82cb6c118099944a499316a
Timo Schilling <timo@schilling.io>
2012-12-17 22:16:20 +0100

    add RequestStore.init with alias as clear

--- test/request_store_test.rb
+++ test/request_store_test.rb
@@ -3,6 +3,18 @@ require 'minitest/autorun'
 require 'request_store'
 
 class RequestStoreTest < Minitest::Unit::TestCase
+  def test_init_with_hash
+    RequestStore.init_or_clear
+    assert_equal Hash.new, RequestStore.store
+  end
+
+  def test_clear
+    RequestStore.init_or_clear
+    RequestStore.store[:foo] = 1
+    RequestStore.init_or_clear
+    assert_equal Hash.new, RequestStore.store
+  end
+
   def test_quacks_like_hash
     Thread.current[:request_store] = {}
     RequestStore.store[:foo] = 1
