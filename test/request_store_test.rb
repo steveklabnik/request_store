2f4ccb702183a2253ee4716c3d0c0a42eb3251e2
Timo Schilling <timo@schilling.io>
2012-12-17 14:42:03 +0100

    init Thread.current[:request_store] with {} in the tests

--- test/request_store_test.rb
+++ test/request_store_test.rb
@@ -4,12 +4,14 @@ require 'request_store'
 
 class RequestStoreTest < Minitest::Unit::TestCase
   def test_quacks_like_hash
+    Thread.current[:request_store] = {}
     RequestStore.store[:foo] = 1
     assert_equal 1, RequestStore.store[:foo]
     assert_equal 1, RequestStore.store.fetch(:foo)
   end
 
   def test_delegates_to_thread
+    Thread.current[:request_store] = {}
     RequestStore.store[:foo] = 1
     assert_equal 1, Thread.current[:request_store][:foo]
   end
