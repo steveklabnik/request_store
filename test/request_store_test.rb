78afe066f4bf7e94cfd3fe893cc299a138778018
Timo Schilling <timo@schilling.io>
2012-12-17 22:16:56 +0100

    use RequestStore.init/clear in tests and middleware

--- test/request_store_test.rb
+++ test/request_store_test.rb
@@ -16,14 +16,14 @@ class RequestStoreTest < Minitest::Unit::TestCase
   end
 
   def test_quacks_like_hash
-    Thread.current[:request_store] = {}
+    RequestStore.init_or_clear
     RequestStore.store[:foo] = 1
     assert_equal 1, RequestStore.store[:foo]
     assert_equal 1, RequestStore.store.fetch(:foo)
   end
 
   def test_delegates_to_thread
-    Thread.current[:request_store] = {}
+    RequestStore.init_or_clear
     RequestStore.store[:foo] = 1
     assert_equal 1, Thread.current[:request_store][:foo]
   end
