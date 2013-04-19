149e6bbd5b1a3f6b6318d2418e137d6457922643
Sebastien Vrillaud <kochka@gmail.com>
2013-04-19 13:40:47 +0200

    Add convenience methods to mimic Rails.cache interface

--- test/request_store_test.rb
+++ test/request_store_test.rb
@@ -27,6 +27,27 @@ class RequestStoreTest < Minitest::Unit::TestCase
     assert_equal 1, RequestStore.store.fetch(:foo)
   end
 
+  def test_read
+    RequestStore.clear!
+    RequestStore.store[:foo] = 1
+    assert_equal 1, RequestStore.read(:foo)
+    assert_equal 1, RequestStore[:foo]
+  end
+
+  def test_write
+    RequestStore.clear!
+    RequestStore.write(:foo, 1)
+    assert_equal 1, RequestStore.store[:foo]
+    RequestStore[:foo] = 2
+    assert_equal 2, RequestStore.store[:foo]
+  end
+
+  def test_fetch
+    RequestStore.clear!
+    assert_equal 2, RequestStore.fetch(:foo) { 1 + 1 }
+    assert_equal 2, RequestStore.fetch(:foo) { 2 + 2 }
+  end
+
   def test_delegates_to_thread
     RequestStore.clear!
     RequestStore.store[:foo] = 1
