fde2d3ecb5dd70be3055cfcf59ede2b2977abc69
Americo Duarte <americodls@gmail.com>
2015-01-19 22:15:17 -0200

    Extending self for clarity

--- lib/request_store.rb
+++ lib/request_store.rb
@@ -4,26 +4,25 @@ require "request_store/railtie" if defined?(Rails::Railtie)
 require "forwardable"
 
 module RequestStore
-  class << self
-    extend Forwardable
+  extend self
+  extend Forwardable
 
-    def_delegators :store, :[], :[]=, :delete, :key?
+  def_delegators :store, :[], :[]=, :delete, :key?
 
-    alias_method :read, :[]
-    alias_method :write, :[]=
-    alias_method :exist?, :key?
+  alias_method :read, :[]
+  alias_method :write, :[]=
+  alias_method :exist?, :key?
 
-    def store
-      Thread.current[:request_store] ||= {}
-    end
+  def store
+    Thread.current[:request_store] ||= {}
+  end
 
-    def clear!
-      Thread.current[:request_store] = {}
-    end
+  def clear!
+    Thread.current[:request_store] = {}
+  end
 
-    def fetch(key, &block)
-      store[key] = yield unless exist?(key)
-      store[key]
-    end
+  def fetch(key, &block)
+    store[key] = yield unless exist?(key)
+    store[key]
   end
 end
