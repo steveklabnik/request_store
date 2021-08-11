2526d45104e6662dad6eafe34879c5a61854156c
Alex Ghiculescu <alex@tanda.co>
2021-08-11 14:04:00 -0500

    Fix fatal crash when a const is returned

--- lib/request_store/middleware.rb
+++ lib/request_store/middleware.rb
@@ -16,12 +16,15 @@ module RequestStore
     def call(env)
       RequestStore.begin!
 
-      response = @app.call(env)
+      status, headers, body = @app.call(env)
+      returned = true
 
-      returned = response << Rack::BodyProxy.new(response.pop) do
+      body = Rack::BodyProxy.new(body) do
         RequestStore.end!
         RequestStore.clear!
       end
+
+      [status, headers, body]
     ensure
       unless returned
         RequestStore.end!
